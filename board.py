bd4907b05f5ead7f641e356c094e68b9a80ec6e4
charley <19979476@qq.com>
2019-05-28 11:42:47 +0800

    add new units

--- board.py
+++ board.py
@@ -6,7 +6,9 @@ def get_wire_status():
     return status
 
 pygame.init()
-screen=pygame.display.set_mode((480,320),pygame.FULLSCREEN,24)
+x=pygame.FULLSCREEN
+x=1
+screen=pygame.display.set_mode((480,320),x,24)
 
 
 background=pygame.image.load('背景.png')
@@ -22,16 +24,17 @@ while not done:
 
     status=get_wire_status()
     for i,s in enumerate(status):
-        if i<len(status)/2:
+        if i<int(len(status)/2)+1:
+            print(int(len(status)/2)+1)
             if s=='0':
-                screen.blit(show_pic_white,(44+i*53,135))
+                screen.blit(show_pic_white,(33+i*44,135))
             elif s=='1':
-                screen.blit(show_pic_black,(44+i*53,135))
-        if i>=len(status)/2:
+                screen.blit(show_pic_black,(33+i*44,135))
+        if i>=int(len(status)/2)+1:
             if s=='0':
-                screen.blit(show_pic_white,(44+(i-len(status)/2)*53,215))
+                screen.blit(show_pic_white,(33+(i-(int(len(status)/2)+1))*44,225))
             elif s=='1':
-                screen.blit(show_pic_black,(44+(i-len(status)/2)*53,215))
+                screen.blit(show_pic_black,(33+(i-(int(len(status)/2)+1))*44,225))
             
     pygame.display.update() 
 
